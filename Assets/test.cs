5dab8a88d901e5bfe7f7f17e0b2a54914951ac26
rui miyazaki <hanano.jack@gmail.com>
2020-02-26 17:54:22 +0900

    課題再提出
    条件３が表記されるようにしました。

--- Assets/test.cs
+++ Assets/test.cs
@@ -19,26 +19,13 @@ public class test : MonoBehaviour {
 		{
 			Debug.Log(points[i]);
 
-		}
+        }
 
-	}
+		int[] points2= { 1, 2, 3, 4, 5 };
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
-public class test2: MonoBehaviour
-{
-
-	// Use this for initialization
-		void Start()
-		{
-			int[] points = { 1, 2, 3, 4, 5 };
-
-			for (int i = points.Length - 1; 0 <= i; i--)
+			for (int i = points2.Length - 1; 0 <= i; i--)
 			{
-				Debug.Log(points[i]);
+				Debug.Log(points2[i]);
 
 			}
 
